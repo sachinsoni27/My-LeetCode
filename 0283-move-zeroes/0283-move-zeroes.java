d12779648d6990e91f1f8755dffa2bc608e2288f
Sachin soni <sachinsoniofficial2003@gmail.com>
2026-06-22 15:34:21 +0530

    Time: 1 ms (99.82%), Space: 47.9 MB (19.63%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+    int position = 0;
+    for( int num : nums){
+        if( num != 0){
+            nums[position++]= num;
+        }
+
+    }
+    while( position < nums.length ){
+        nums[position++] = 0;
+    }
+    }
+}
