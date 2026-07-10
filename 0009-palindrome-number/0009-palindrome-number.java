00db1aaadf9854ea03ce1a6623587f21563bcc1e
Sachin soni <sachinsoniofficial2003@gmail.com>
2026-07-10 14:09:29 +0530

    Time: 5 ms (80.32%), Space: 45.8 MB (72.22%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int org = x ;
+        int rev = 0;
+    while(x>0){
+            rev = rev*10 + x%10;
+            x= x/10;
+        }
+        if(rev == org){
+            return true;
+        }else{ return false;}
+    }
+}
