8bd074d70af4a0bff8808f5133ca546c4991700b
Sachin soni <sachinsoniofficial2003@gmail.com>
2026-08-09 18:04:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.java
+++ Difficulty: Basic/Reverse a String/reverse-a-string.java
@@ -1,8 +1,29 @@
-class Solution {
+/*class Solution {
     public static String reverseString(String s) {
         if (s == null) {
             return null;
         }
         return new StringBuilder(s).reverse().toString();
     }
-}   
+}
+*/
+
+class Solution {
+    public static String reverseString(String s) {
+        char[] arr = s.toCharArray();
+
+        int i = 0;
+        int j = arr.length - 1;
+
+        while (i < j) {
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+        }
+
+        return new String(arr);
+    }
+}
