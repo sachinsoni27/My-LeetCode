e37b71e4e6322a9b871aaee561d31f72d4ea9a47
Sachin soni <sachinsoniofficial2003@gmail.com>
2026-08-22 11:36:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of Natural Numbers/sum-of-natural-numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+     System.out.println(n*(n+1)/2);
+        // code here
+      
+        
+
+        
+    }
+  
+}
