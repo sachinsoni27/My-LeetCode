6fbbc8ed1bed4c756093039a970fd1f241230583
Sachin soni <sachinsoniofficial2003@gmail.com>
2026-09-05 17:26:50 +0530

    Time: 0 ms (100%), Space: 43.9 MB (43.64%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean searchMatrix(int[][] arr, int target) {
+
+        int m = arr.length;
+        int n = arr[0].length;
+
+       
+        int low = 0;
+        int high = m*n- 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+            int row = mid/n;
+            int col = mid%n;
+
+            if (arr[row][col] == target) {
+                return true;
+            }
+
+            if (arr[row][col] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
